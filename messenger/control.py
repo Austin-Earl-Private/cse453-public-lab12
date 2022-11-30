71c17a256a042d5c30823c316553adcb42782477
Austin-Earl-Private <the.qmind2020@gmail.com>
2022-11-29 21:52:48 -0700

    Update control.py

--- messenger/control.py
+++ messenger/control.py
@@ -3,8 +3,25 @@
 #    CONTROL
 # Author:
 #    Br. Helfrich, Kyle Mueller, <your name here if you made a change>
-# Summary: 
+# Summary:
 #    This class stores the notion of Bell-LaPadula
 ########################################################################
 
 # you may need to put something here...
+from enum import Enum
+
+
+class Control(Enum):
+    UNCLASSIFIED = 1
+    PUBLIC = 2
+    CONFIDENTIAL = 3
+    SECRET = 4
+    TOP_SECRET = 5
+
+
+def securityConditionRead(assetControl: Control, subjectControl: Control):
+    return subjectControl >= assetControl
+
+
+def securityConditionWrite(assetControl: Control, subjectControl: Control):
+    return subjectControl <= assetControl
